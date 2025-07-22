8938b1a0d1ff557103af27db9d86dc746131b13b
Daniel Lindestad <lindestad@proton.me>
2025-07-22 22:14:16 +0200

    Improved the symlink handling logic. Added symlink for ncspot. Changed helix links to be individual files.

--- symlink.ps1
+++ symlink.ps1
@@ -5,11 +5,13 @@ $Local = [Environment]::GetFolderPath('LocalApplicationData')      # %LocalAppDa
 
 # Use array of objects to allow duplicate sources with different destinations
 $Links = @(
-    @{ Src = "$Dotfiles\config\helix"; Dst = Join-Path $Roaming "helix" },
+    @{ Src = "$Dotfiles\config\helix\config.toml"; Dst = Join-Path $Roaming "helix\config.toml" },
+    @{ Src = "$Dotfiles\config\helix\languages.toml"; Dst = Join-Path $Roaming "helix\languages.toml" },
     @{ Src = "$Dotfiles\shells\config.nu"; Dst = Join-Path $Roaming "nushell\config.nu" },
     @{ Src = "$Dotfiles\config\starship\starship.toml"; Dst = Join-Path $Local "clink\starship.lua" },       # For CMD/Clink
     @{ Src = "$Dotfiles\config\starship\starship.toml"; Dst = Join-Path $Roaming "starship\config.toml" },   # For Starship in PowerShell
     @{ Src = "$Dotfiles\config\yazi"; Dst = Join-Path $Roaming "yazi\config" }
+    @{ Src = "$Dotfiles\config\ncspot\config.toml"; Dst = Join-Path $Roaming "ncspot\config.toml" }
     # @{ Src = "$Dotfiles\shells\.zshrc";                 Dst = Join-Path $UserHome ".zshrc" },
     # @{ Src = "$Dotfiles\shells\.bashrc";                Dst = Join-Path $UserHome ".bashrc" }
 )
@@ -23,10 +25,24 @@ foreach ($item in $Links) {
         New-Item -ItemType Directory -Path $dstDir -Force | Out-Null
     }
 
+    $needsLink = $true
     if (Test-Path $dst) {
-        Remove-Item $dst -Force
+        try {
+            $existing = Get-Item $dst -Force
+            if ($existing.LinkType -eq 'SymbolicLink' -and $existing.Target -eq $src) {
+                Write-Host "Already linked: $dst -> $src"
+                $needsLink = $false
+            } else {
+                Remove-Item $dst -Force -Recurse
+            }
+        } catch {
+            # Just in case it's not a link and throws during Get-Item.Target
+            Remove-Item $dst -Force -Recurse
+        }
     }
 
-    New-Item -ItemType SymbolicLink -Path $dst -Target $src | Out-Null
-    Write-Host "Linked $src --> $dst"
+    if ($needsLink) {
+        New-Item -ItemType SymbolicLink -Path $dst -Target $src | Out-Null
+        Write-Host "Linked $src --> $dst"
+    }
 }
