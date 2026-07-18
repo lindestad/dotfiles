e1dc72fe6fa9f8328c39f54c9383d160b296657b
Daniel Lindestad <lindestad@proton.me>
2026-07-19 00:33:32 +0200

    configure niri-zvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,6 @@
 require("config.lazy")
+
+local niri_zvim_adapter = vim.fn.stdpath("data") .. "/site/lua/niri-zvim"
+if vim.fn.isdirectory(niri_zvim_adapter) == 1 then
+  require("niri-zvim").setup()
+end
